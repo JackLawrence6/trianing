c0826e180c530c85b9e5595e0e33903d0f04d7d0
Gachen <576512870@qq.com>
2022-06-08 09:22:03 -0400

    OK

---
+++ src/components/myIndex.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="de">
+        {{PageText}}
+    </div>
+</template>
+
+<script>
+
+export default{
+    name:'myIndex',
+    data(){
+        return {
+            PageText:'New Page',
+        }
+    },
+
+}
+</script>
+
+<style scoped>
+
+.de{
+    color: brown;
+    border: 1px solid black;
+    border-radius: 5px;
+    background-color: #006266;
+}
+
+</style>
